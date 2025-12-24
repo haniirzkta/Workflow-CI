7e3e99869827664f82b3194745a20c9fc056933d
haniirzkta <Hanifahrzktaa22@gmail.com>
2025-12-24 10:41:02 +0700

    Fix MLProject update Workflow

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -8,12 +8,13 @@ import os
 
 def main(data_path):
     df = pd.read_csv(data_path)
-
     target_col = "target"  # sesuaikan dengan kolom target di dataset
     X = df.drop(target_col, axis=1)
     y = df[target_col]
 
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=42
+    )
 
     with mlflow.start_run():
         model = LogisticRegression(max_iter=500)
