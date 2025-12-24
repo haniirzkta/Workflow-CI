05e3712e930c052d878eb5bd38d605a7a6a2d8e8
haniirzkta <Hanifahrzktaa22@gmail.com>
2025-12-24 10:51:35 +0700

    Fix MLProject

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -4,11 +4,11 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 import mlflow
 import mlflow.sklearn
-import os
 
 def main(data_path):
     df = pd.read_csv(data_path)
-    target_col = "target"  # sesuaikan dengan kolom target di dataset
+
+    target_col = "target"
     X = df.drop(target_col, axis=1)
     y = df[target_col]
 
@@ -16,19 +16,14 @@ def main(data_path):
         X, y, test_size=0.2, random_state=42
     )
 
-    with mlflow.start_run():
-        model = LogisticRegression(max_iter=500)
-        model.fit(X_train, y_train)
-        score = model.score(X_test, y_test)
-        print(f"Accuracy: {score}")
+    # AUTLOG
+    mlflow.sklearn.autolog()
 
-        mlflow.log_metric("accuracy", score)
-        mlflow.sklearn.log_model(model, "model")
+    model = LogisticRegression(max_iter=500)
+    model.fit(X_train, y_train)
 
-        # Save model ke folder artifacts
-        model_dir = "artifacts"
-        os.makedirs(model_dir, exist_ok=True)
-        mlflow.sklearn.save_model(model, os.path.join(model_dir, "model"))
+    score = model.score(X_test, y_test)
+    print(f"Accuracy: {score}")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
